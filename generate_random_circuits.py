0097849ff366ce4ec0baccf41d121740ae23d33c
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-09-07 18:59:59 -0400

    Came up with rules for valid random 1 qubit operations after testing with implemented random 1 qubit circuit generation code

---
+++ generate_random_circuits.py
@@ -0,0 +1,174 @@
+from qiskit import QuantumCircuit, ClassicalRegister
+import numpy as np
+import pyzx as zx
+import time
+from datetime import datetime
+import sys
+from qiskit_aer import AerSimulator
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_ibm_runtime import Sampler
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit.transpiler import generate_preset_pass_manager, PassManager
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit.qasm2 import dumps
+from qiskit.circuit import library as lib
+from qiskit.transpiler.passes import (
+    ALAPScheduleAnalysis,
+    InverseCancellation,
+    PadDynamicalDecoupling,
+)
+import random
+
+def apply_zx_calc(circuit, n: int=3):
+    #convert to qasm intermediary format
+    with open(f"teleportation/{n}.txt", "w") as f:
+        qasm_circuit = str(dumps(circuit))
+        f.write(qasm_circuit)
+    with open(f"teleportation/{n}.txt", "r") as f:
+        lines = f.read().split('\n')
+        format_lines = ""
+        for l in lines:
+            if 'creg' not in l and 'measure' not in l and 'barrier' not in l:
+                format_lines += (l+'\n')
+    qasm_file_name = f"teleportation/{n}.qasm"
+    with open(qasm_file_name, "w") as f:
+        qasm_circuit = QuantumCircuit.from_qasm_str(format_lines)
+        formatted = dumps(qasm_circuit)
+        f.write(formatted)
+    #apply zx calculus
+    zx_qasm_circuit = zx.Circuit.load(qasm_file_name)
+    graph = zx_qasm_circuit.to_graph(compress_rows=True)
+    print("ZX-Calculus Reduction Steps:")
+    print("----------------------------")
+    zx.full_reduce(graph, quiet=False)
+    print("\n")
+    graph.normalize()
+    optimized_circuit = zx.extract_circuit(graph.copy())
+    g = zx_qasm_circuit
+    p = optimized_circuit
+    opt_qasm = optimized_circuit.to_qasm()
+    opt_circuit = QuantumCircuit.from_qasm_str(opt_qasm)
+    stats_circuit('Pre ZX-calculus optimized stats: ', g)
+    stats_circuit('Post ZX-calculus optimized stats: ', p)
+    # print("optimization validation check: ", zx.compare_tensors(g, p))
+    # print("optimization validation check: ", zx_qasm_circuit.verify_equality(optimized_circuit, up_to_swaps=False, up_to_global_phase=True))
+    return opt_circuit
+
+def output_circuit(message, circuit):
+    print(message)
+    print(circuit.draw(output='text'))
+    print('\n')
+
+
+def stats_circuit(message, circuit):
+    print(message)
+    print(circuit.to_basic_gates().stats())
+    print('\n')
+
+one_qubit_ops = ["HGATE", "IGATE", "PHASEGATE", "RGATE", "RXGATE", "RYGATE", "RZGATE", "SGATE", "SDGGATE", "SXGATE", "SXDGGATE", "TGATE", "TDGGATE", "UGATE", "U1GATE", "U2GATE", "XGATE", "YGATE", "ZGATE", "NOGATE"]
+def generate_random(n_qubits):
+    circuit = QuantumCircuit(n_qubits, n_qubits)
+    #use a seed for replication?
+    #probably randomly generate phi, theta, etc. angle values that are fixed per circuit
+    #extract the below to a one_q_op function that can be selected as one of the random options according to the drawn out rules?
+    #u1, u2, u3 are deprecated and are replaced with specific angle measurements to existing gates
+    #u3 has been replaced by the pure u gate
+    for i in range(n_qubits):
+        random_idx = random.randint(0,19)
+        selected_op = one_qubit_ops[random_idx]
+        if selected_op == "HGATE":
+            circuit.h(i)
+        elif selected_op == "IGATE":
+            circuit.id(i)
+        elif selected_op == "PHASEGATE":
+            circuit.p(1.5, i)
+        elif selected_op == "RXGATE":
+            circuit.rx(1.5, i)
+        elif selected_op == "RYGATE":
+            circuit.ry(1.5, i)
+        elif selected_op == "RZGATE":
+            circuit.rz(1.5, i)
+        elif selected_op == "SGATE":
+            circuit.s(i)
+        elif selected_op == "SDGGATE":
+            circuit.sdg(i)
+        elif selected_op == "SXGATE":
+            circuit.sx(i)
+        elif selected_op == "SXDGGATE":
+            circuit.sxdg(i)
+        elif selected_op == "TGATE":
+            circuit.t(i)
+        elif selected_op == "TDGGATE":
+            circuit.tdg(i)
+        elif selected_op == "UGATE":
+            circuit.u(1.5, 2.5, 3.5, i)
+        elif selected_op == "U1GATE":
+            circuit.u(0, 0, 1.5, i)
+        elif selected_op == "U2GATE":
+            circuit.u(np.pi/2, 1.5, 1.5, i)
+        elif selected_op == "XGATE":
+            circuit.x(i)
+        elif selected_op == "YGATE":
+            circuit.y(i)
+        elif selected_op == "ZGATE":
+            circuit.z(i)
+    print(circuit)
+    return circuit
+
+def controller(n_qubits:int=3, hardware: str='s', opt_method:str=''):
+    # circuit = QuantumCircuit(n_qubits,n_qubits)
+    circuit = generate_random(n_qubits)
+    #insert random gates here/use randomly generated circuit
+    #measure qubits 0 and 1 into classical bits 0 and 1
+    # circuit.measure([0,1],[0,1])
+
+    result, start_time, end_time, correct, total=None, None, None, 0, 0
+    if hardware=='r':
+        message=f'Pre {opt_method} optimized circuit: '
+        opt_level = 0
+        layout_method = ''
+        routing_method = ''
+        translation_method = ''
+        if opt_method=='ZX':
+            circuit = apply_zx_calc(circuit, n_qubits)
+            circuit.add_register(ClassicalRegister(n))
+            circuit.measure(range(n), range(n))
+            message='Post ZX-calculus optimized circuit: '
+        print('Circuit stats: ', circuit.count_ops())
+        output_circuit(message, circuit)
+        service = QiskitRuntimeService()
+        backend = service.backend("ibm_brisbane")
+        sampler = Sampler(mode=backend)
+        pass_manager = generate_preset_pass_manager(
+            optimization_level=opt_level, backend=backend, layout_method=layout_method, routing_method=routing_method, translation_method=translation_method
+        )
+        transpiled = pass_manager.run(circuit)
+        print('Transpiled circuit stats: ', transpiled.count_ops())
+        # output_circuit(f'Transpiled {message}', transpiled)
+        job = sampler.run([(transpiled,)])
+        result = job.result()[0].join_data().get_counts()
+        start_time = datetime.strptime(str(job.result().metadata['execution']['execution_spans']['__value__']['spans'][0].start), '%Y-%m-%d %H:%M:%S.%f')
+        end_time = datetime.strptime(str(job.result().metadata['execution']['execution_spans']['__value__']['spans'][0].stop), '%Y-%m-%d %H:%M:%S.%f')
+    elif hardware=='s':
+        message='Pre ZX-calculus optimized circuit: '
+        if opt_method=='ZX':
+            circuit = apply_zx_calc(circuit, n_qubits)
+            circuit.add_register(ClassicalRegister(n))
+            circuit.measure(range(n), range(n))
+            message='Post ZX-calculus optimized circuit: '
+        print("Circuit stats: ", circuit.count_ops())
+        output_circuit(message, circuit)
+        # simulator = AerSimulator(method="matrix_product_state")
+        # circuit.measure(2,2)
+        # start_time = time.time()
+        # result = simulator.run(circuit, shots=1000).result().get_counts()
+        # end_time = time.time()
+
+if __name__=='__main__':
+    n = int(sys.argv[1])
+    hardware = sys.argv[2]
+    if len(sys.argv)>3:
+        opt_method = sys.argv[3]
+        controller(n, hardware, opt_method)
+    else:
+        controller(n, hardware, '')
