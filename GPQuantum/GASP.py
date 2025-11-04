e4f544c96d3c8eb1de026ffdcda0660f1b3ff304
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-11-03 20:44:32 -0500

    Implementation of steps c) and d) of the GASP experiment/algorithm.

--- GPQuantum/GASP.py
+++ GPQuantum/GASP.py
@@ -13,7 +13,7 @@ import numpy as np
 #gene = [q_ti, G_i, q_ci, theta]
 gene_gates = ["R_X", "R_Y", "R_Z", "CNOT"]
 #temp testing value
-init_pop_size = 10
+init_pop_size = 100
 #n determines the W-state (target) and size of individuals
 n = 6 
 
@@ -38,29 +38,62 @@ def generate_init_pop():
     """
     Each individual P_i = [gene_1, gene_2, ..., gene_n]
     """
-    init_pop = []
+    init_pop_circuits = []
+    init_pop_genes = []
     for i in range(init_pop_size):
         individual = QuantumCircuit(n, n)
+        individual_genes = []
         j = 0
         while j < n:
             g = gene_gates[random.randint(0, 3)]
             if g == "R_X":
                 individual.rx(1.5, j)
+                individual_genes += [g]
                 j += 1
             elif g == "R_Y":
                 individual.ry(1.5, j)
+                individual_genes += [g]
                 j += 1
             elif g == "R_Z":
                 individual.rz(1.5, j)
+                individual_genes += [g]
                 j += 1
             elif g == "CNOT":
                 if j+1 == n:
                     continue
                 else:
                     individual.cx(j, j+1)
+                    individual_genes += [g]
                     j += 2
-        init_pop += [individual]
-    return init_pop
+        init_pop_circuits += [individual]
+        init_pop_genes += [individual_genes]
+    return init_pop_circuits, init_pop_genes
+
+def create_circuits(population):
+    """
+    Turns the gene representations of population individuals into actual Qiskit circuits.
+    """
+    circuit_representations = []
+    for circuit in population:
+        individual = QuantumCircuit(n, n)
+        for j in range(len(circuit)):
+            if circuit[j] == "R_X":
+                individual.rx(1.5, j)
+                j += 1
+            elif circuit[j] == "R_Y":
+                individual.ry(1.5, j)
+                j += 1
+            elif circuit[j] == "R_Z":
+                individual.rz(1.5, j)
+                j += 1
+            elif circuit[j] == "CNOT":
+                if j+1 == n:
+                    continue
+                else:
+                    individual.cx(j, j+1)
+                    j += 2
+        circuit_representations += [individual]
+    return circuit_representations
 
 
 def get_circuit_state(circuit):
@@ -98,9 +131,70 @@ def run_generation(W_state, generation):
         evaluations += [[individual, calculate_fitness(bitstring_a, bitstring_b).item()]]
     return evaluations
 
+def one_point_crossover(parent_1, parent_2):
+    """
+    1-point crossover (50% crossover) 2-parent breeding.
+    """
+    half_1_idx = len(parent_1)//2
+    half_2_idx = len(parent_2)//2
+    child = parent_1[:half_1_idx] + parent_2[half_2_idx:]
+    return child
+
+def breed_population(population):
+    """
+    Apply the chosen crossover method to the entire population. Result population is the same size
+    as the input population.
+    """
+    new_generation = []
+    while len(new_generation) < len(population):
+        parent_1_idx = random.randint(0, len(population)-1)
+        if parent_1_idx + 1 < len(population):
+            parent_2_idx = parent_1_idx+1
+        else:
+            parent_2_idx = parent_1_idx-1
+        child = one_point_crossover(population[parent_1_idx], population[parent_2_idx])
+        new_generation += [child]
+    return new_generation
+
+def mutate_individual(individual):
+    """
+    Mutates a random gene in the individual. Should probably make the number of mutated genes also variable.
+    """
+    idx_to_mutate = random.randint(0, len(individual)-1)
+    gate = individual[idx_to_mutate]
+    if gate == "R_X" or gate == "R_Y" or gate == "R_Z":
+        individual[idx_to_mutate] = gene_gates[random.randint(0, len(gene_gates)-2)]
+    elif gate == "CNOT":
+        individual[idx_to_mutate] = gene_gates[random.randint(0, len(gene_gates)-1)]
+    return individual
+
+def mutate_population(population):
+    """
+    The entire input population is mutated with probability mutation_rate.
+    """
+    mutation_rate = 0.05
+    mutated_population = []
+    n_mutate = int(len(population)*mutation_rate)
+    idxs_mutate = []
+    i = 0
+    while i < n_mutate:
+        idx = random.randint(0, len(population)-1)
+        if idx not in idxs_mutate:
+            idxs_mutate += [idx]
+            i += 1
+    for i in range(len(population)):
+        if idx in idxs_mutate:
+            mutated_individual = mutate_individual(population[i])
+            mutated_population += [mutated_individual]
+        else:
+            mutated_population += [population[i]]
+    return mutated_population
+
 
 if __name__=='__main__':
     W_state = get_circuit_state(create_W_state(n))
-    init_population = generate_init_pop()
-    run_generation(W_state, init_population)
-    
+    init_population, init_pop_genes = generate_init_pop()
+    bred = breed_population(init_pop_genes)
+    mutated = mutate_population(bred)
+    mutated_population = create_circuits(mutated)
+    print("FIRST GENERATION: ", run_generation(W_state, mutated_population), ": ", len(mutated_population))
