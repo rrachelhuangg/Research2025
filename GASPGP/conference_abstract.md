aca7da4921f68cbce50dfc149fbaada6366e18c4
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2026-03-04 13:42:36 -0500

    Experiment analysis framework for comparing two experiments.

---
+++ GASPGP/conference_abstract.md
@@ -0,0 +1,7 @@
+Title: Implementing a Quantum Circuit Optimizer Using Genetic Programming and ZX-Calculus
+
+Abstract: Quantum computers are implemented on networks of subatomic particles and are thus extremely fragile, causing circuit error to grow exponentially as their size and complexity increase. ZX-calculus is an optimization technique that applies a set of simplification rules to circuit operations, removing unnecessary components while maintaining functionality. This project implements a quantum circuit optimizer that generates target circuits and optimizes them for ZX-calculus using genetic programming. Generated circuits are evaluated for their accuracy, optimization time, length, and suitability for ZX-calculus. Implemented circuits were evaluated by running experiments on the Torino QPU and its simulator provided by the IBM Quantum Platform. 
+
+
+
+
---
+++ GASPGP/experiment_analysis.py
@@ -0,0 +1,43 @@
+from experiment_result_gene_circuits import qv_no_zx, qv_yes_zx
+from GASP_steps import individual_to_circuit, target_state_circuit
+from zx_helper import assign_zx_value
+from qiskit.circuit.library import QuantumVolume
+from qiskit import transpile
+from qiskit_aer import AerSimulator
+from qiskit.quantum_info import Statevector
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit.transpiler import generate_preset_pass_manager
+
+target_circuit = target_state_circuit
+print("TARGET CIRCUIT: ", target_circuit.draw(output='text'))
+
+no_zx_circuit = individual_to_circuit(qv_no_zx)
+yes_zx_circuit = individual_to_circuit(qv_yes_zx)
+
+no_zx_circuit = individual_to_circuit(qv_no_zx)
+print("QV NO ZX CIRCUIT: ", no_zx_circuit.draw(output='text'))
+
+yes_zx_circuit = individual_to_circuit(qv_yes_zx)
+print("QV YES ZX CIRCUIT: ", yes_zx_circuit.draw(output='text'))
+
+#stuff to compare
+#zx-calc-ness
+print("NO ZX CIRCUIT ZX VALUE: ", assign_zx_value(no_zx_circuit))
+print("YES ZX CIRCUIT ZX VALUE: ", assign_zx_value(yes_zx_circuit))
+#length
+print("NO ZX CIRCUIT LENGTH: ", len(no_zx_circuit))
+print("NO ZX CIRCUIT GATE STATS:", no_zx_circuit.count_ops())
+print("YES ZX CIRCUIT LENGTH: ", len(yes_zx_circuit))
+print("YES ZX CIRCUIT GATE STATS: ", yes_zx_circuit.count_ops())
+#speed
+service = QiskitRuntimeService()
+backend = service.backend("ibm_torino")
+simulator = AerSimulator.from_backend(backend) #simulator modeled after the chosen hardware backend
+transpiled_no_zx = transpile(no_zx_circuit, simulator)
+print("NO ZX CIRCUIT RUN RESULTS: ", simulator.run(transpiled_no_zx, shots=1000).result())
+transpiled_yes_zx = transpile(yes_zx_circuit, simulator)
+print("YES ZX CIRCUIT RUN RESULTS: ", simulator.run(transpiled_yes_zx, shots=1000).result())
+#functionality (how to use qv circuit)
+#fitness (already calculated)
+
+
